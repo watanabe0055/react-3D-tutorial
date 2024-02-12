2feb0a6310738c72a988993306ebe0ae61097451
watanabe0055 <mailok1212+github@gmail.com>
2024-02-12 14:52:02 +0900

    island comp

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,51 @@
 import { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import Loader from "../components/Loader";
+import Island from "../models/lsland";
+
+{
+  /* <div className="absolute right-0 z-10 flex items-center justify-center top-28 left-8">
+        POPUP
+      </div> */
+}
 
 const Home = () => {
+  const adjustIslandForScreenSize = () => {
+    let screenScale = null;
+    let screenPosition = [0, -6.5, -43];
+    let rotation = [0.1, 4.7, 0];
+
+    if (window.innerWidth < 768) {
+      screenScale = [0.9, 0.9, 0.9];
+    } else {
+      screenScale = [1, 1, 1];
+    }
+
+    return [screenScale, screenPosition, rotation];
+  };
+
+  const [isLandScale, islandPosition, isRotation] = adjustIslandForScreenSize();
+
   return (
     <section className="relative w-full h-screen">
-      {/* <div className="absolute right-0 z-10 flex items-center justify-center top-28 left-8">
-        POPUP
-      </div> */}
       <Canvas
         className="w-full h-screen bg-transparent"
         camera={{ near: 0.1, far: 1000 }}
       >
         <Suspense fallback={<Loader />}>
-          <directionalLight />
-          <ambientLight />
-          <pointLight />
-          <spotLight />
-          <hemisphereLight />
+          <directionalLight position={[1, 1, 1]} intensity={2} />
+          <ambientLight intensity={0.5} />
+          <hemisphereLight
+            skyColor="#b1e1ff"
+            groundColor="#000"
+            intensity={1}
+          />
+
+          <Island
+            scale={isLandScale}
+            position={islandPosition}
+            rotation={isRotation}
+          />
         </Suspense>
       </Canvas>
     </section>
